0f7f0984ff6c3f765adc6f0690a5ad737499f751
hashimotoakihiko <jap21.wave@gmail.com>
2019-12-11 17:29:34 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
 
 
   root "messages#index"
 end
-=======
-  get 'messages/index'
-
-  root "messages#index"
-end
->>>>>>> Stashed changes
