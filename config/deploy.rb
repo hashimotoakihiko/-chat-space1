52991b88b71cb82ac9ad78b612516829f7c14653
hashimotoakihiko <jap21.wave@gmail.com>
2020-01-06 11:52:23 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['hashimotoakihiko'],
-                  keys: ['hashimotoakihiko'] 
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/hashimotoakihiko.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
